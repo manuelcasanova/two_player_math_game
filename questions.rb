aa159ed64745236b97d4fbe89f259da63eb739dd
Manuel Casanova <manucasanova@hotmail.com>
2022-06-29 11:07:15 -0700

    Pass current player from initialize to ask

--- questions.rb
+++ questions.rb
@@ -7,6 +7,8 @@ class Questions
     @nume2 = rand(1..20)
     @answer = @num1 + @nume2
 
+    @current_player = current_player
+
     puts "#{current_player}: What's #{@num1} plus #{@nume2} ?"
     ask
 
@@ -16,14 +18,17 @@ class Questions
   def ask
 
     @player_answer = gets.chomp
+   
+
 
     if @player_answer == @answer.to_s
-      puts "Correct answer"
-      # puts "#{current_player}, well done, that's the correct answer"
+      #puts "Correct answer"
+      puts "#{@current_player}, well done, that's the correct answer"
       @correct = true
+    
 
     else
-      puts "Incorrect answer"
+      puts "#{@current_player}, Incorrect answer"
       # puts "#{current_player}, nope!, that's a wrong answer"
       @correct = false
 
